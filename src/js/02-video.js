a353559bff7ac0c69e33b9d395e356f29f8689a0
Petr Mazka <petr.mazka@gmail.com>
2022-05-25 20:45:37 +0300

    Hw8
    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+const options = {
+  id: 236203659,
+  width: 640,
+};
+
+const player = new Player('vimeo-player', options);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+setCurrentTime();
+
+function onPlay(data) {
+  localStorage.setItem(CURRENT_TIME_KEY, data.seconds);
+}
+
+function setCurrentTime() {
+  const currentTime = localStorage.getItem(CURRENT_TIME_KEY);
+
+  if (currentTime) {
+    player.setCurrentTime(currentTime);
+  }
+}
